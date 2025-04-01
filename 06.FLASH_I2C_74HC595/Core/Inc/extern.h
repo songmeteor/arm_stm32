9403f186b278f43267fd037670b961b30a4f2692
songmeteor <meteor520@naver.com>
2025-04-01 14:24:28 +0900

    06.FLASH_I2C_74HC595

---
+++ 06.FLASH_I2C_74HC595/Core/Inc/extern.h
@@ -0,0 +1,33 @@
+#ifndef __EXTERN_H
+#define __EXTERN_H
+
+#include "main.h"
+
+//#define COMMAND_NUMBER 20
+//#define COMMAND_LENGHT 40
+
+extern uint8_t rx_data;
+extern UART_HandleTypeDef huart2;
+extern TIM_HandleTypeDef htim2;
+
+//extern volatile uint8_t rx_buff[COMMAND_NUMBER][COMMAND_LENGHT];   // uart0로 부터 들어온 문자를 저장 하는 버퍼(변수)
+//extern volatile int rear; // input index : HAL_UART_RxCpltCallback에서 집어 넣어 주는 index
+//extern volatile int front; //output index
+extern volatile int TIM11_1ms_counter;
+extern volatile int TIM11_1ms_counter2;
+extern t_print o_prt;
+
+extern void led_main(void);
+extern void button_led_toggle_test(void);
+extern void ds1302_main(void);
+extern void pc_command_processing(void);
+extern void set_RTC(char *date_time);
+extern void delay_us(int us);
+
+extern void flash_main();
+extern HAL_StatusTypeDef flash_write (uint32_t *data32, int size);
+extern HAL_StatusTypeDef flash_read(uint32_t *addr32, int size);
+extern void flash_set_time(void);
+extern HAL_StatusTypeDef flash_erase();
+
+#endif /* __EXTERN_H */
---
+++ 06.FLASH_I2C_74HC595/Core/Src/ds1302.c
@@ -0,0 +1,246 @@
+/*
+ * ds1302.c
+ *
+ *  Created on: Mar 28, 2025
+ *      Author: microsoft
+ */
+
+#include <string.h>
+#include <stdio.h>
+#include "extern.h"
+#include "ds1302.h"
+
+
+
+void set_RTC(char *date_time);
+
+void set_RTC(char *date_time)
+{
+	if(strncmp(date_time, "setrtc", 6) == 0)
+	{
+		ds1302.year = ((int)date_time[6] - 48)*10 + ((int)date_time[7] - 48);
+		ds1302.month = ((int)date_time[8] - 48)*10 + ((int)date_time[9] - 48);
+		ds1302.date = ((int)date_time[10] - 48)*10 + ((int)date_time[11] - 48);
+		ds1302.hours = ((int)date_time[12] - 48)*10 + ((int)date_time[13] - 48);
+		ds1302.minutes = ((int)date_time[14] - 48)*10 + ((int)date_time[15] - 48);
+		ds1302.seconds = ((int)date_time[16] - 48)*10 + ((int)date_time[17] - 48);
+
+		//front = (front + 1) % COMMAND_NUMBER;
+		init_ds1302();
+	}
+}
+
+void ds1302_main()
+{
+	init_date_time();
+	init_gpio_ds1302();
+	flash_set_time();
+	flash_read((uint32_t *)&ds1302, sizeof(ds1302));
+	init_ds1302();
+
+	while(1)
+	{
+		if(TIM11_1ms_counter2>10000)
+		{
+			flash_erase();
+			flash_write((uint32_t *)&ds1302, sizeof(ds1302));
+			TIM11_1ms_counter2=0;
+		}
+		read_time_ds1302();
+		read_date_ds1302();
+		pc_command_processing();
+		//set_RTC((char*)rx_buff[front]);
+		if(TIM11_1ms_counter > 1000)
+		{
+			TIM11_1ms_counter = 0;
+			// 날짜와 시각을 출력
+			if(o_prt.p_rtc)
+			{
+				printf(" %4d-%2d-%2d %2d:%2d:%2d\n",
+						ds1302.year+2000,
+						ds1302.month,
+						ds1302.date,
+						ds1302.hours,
+						ds1302.minutes,
+						ds1302.seconds);
+			}
+		}
+	}
+}
+
+void read_time_ds1302()
+{
+	ds1302.seconds = read_ds1302(ADDR_SECONDS);
+	ds1302.minutes = read_ds1302(ADDR_MINUTES);
+	ds1302.hours = read_ds1302(ADDR_HOURS);
+}
+
+void read_date_ds1302()
+{
+	ds1302.date = read_ds1302(ADDR_DATE);
+	ds1302.month = read_ds1302(ADDR_MONTH);
+	ds1302.dayofweek = read_ds1302(ADDR_DAYOFWEEK);
+	ds1302.year = read_ds1302(ADDR_YEAR);
+}
+
+uint8_t read_ds1302(uint8_t addr)
+{
+	unsigned char data8bits = 0;	// 1bit씩 넘어온 것을 담을 그릇(변수)
+	// 1. CE high
+	HAL_GPIO_WritePin(GPIOA, CE_DS1302_Pin, 1);
+	// 2. addr 전송
+	tx_ds1302(addr+1);				// read address
+	// 3. data를 읽는다.
+	rx_ds1302(&data8bits);			// 싱글 변수 따라서 &로 주소 불러오기
+	// 4. CE low
+	HAL_GPIO_WritePin(GPIOA, CE_DS1302_Pin, 0);
+	// 5. return (bcd to dec)
+	return bcd2dec (data8bits);
+}
+// 1. 입력: bcd
+// ex) 25년의 bcd --> dec
+//     7654 3210
+//     0010 0101
+//       2    5
+//     x10   x1
+//  ===============
+//        25
+uint8_t bcd2dec(uint8_t bcd)
+{
+	uint8_t high, low;
+
+	low = bcd & 0x0f;
+	high = (bcd >> 4) * 10;	// (0010 0101)bcd >> 4 ==> 0000 0010 x10
+
+	return (high+low);
+}
+//dec-->bcd
+// ex) 25
+//    dec		   bcd
+// 0001 1001	0010 0101
+uint8_t dec2bcd(uint8_t dec)
+{
+	uint8_t high, low;
+
+	high = (dec/10) << 4;
+	low = dec %10;
+
+	return (high+low);
+}
+
+void rx_ds1302(unsigned char *pdata)
+{
+	unsigned char temp=0;
+	// IO 포트를 입력 모드로 전환
+	input_dataline_ds1302();
+	// DS1302로 부터 들어온 bit를 LSB부터 8bit를 받아서 temp변수에 저장
+	for (int i=0; i<8; i++)
+	{
+		// 1bit를 읽는다.
+		if (HAL_GPIO_ReadPin(GPIOA, IO_DS1302_Pin))
+		{
+			// 1의 조건만 set
+			temp |= 1 << i;
+		}
+		if(i != 7) clock_ds1302();	// 바로 읽기 때문에 clock을 한다. 대신 7일때 X
+	}
+	*pdata = temp;	// pdata는 data8bits의 주소가 저장
+					// 따라서 *pdata를 하여 data8bits의 주소로 가서 값을 바꾼다
+}
+
+
+void init_ds1302()
+{
+	write_ds1302(ADDR_SECONDS, ds1302.seconds);
+	write_ds1302(ADDR_MINUTES, ds1302.minutes);
+	write_ds1302(ADDR_HOURS, ds1302.hours);
+	write_ds1302(ADDR_DATE, ds1302.date);
+	write_ds1302(ADDR_MONTH, ds1302.month);
+	write_ds1302(ADDR_DAYOFWEEK, ds1302.dayofweek);
+	write_ds1302(ADDR_YEAR, ds1302.year);
+}
+
+void write_ds1302(uint8_t addr, uint8_t data)
+{
+	// 1. CE low ----> high
+	HAL_GPIO_WritePin(GPIOA, CE_DS1302_Pin, 1);
+	// 2. addr 전송
+	tx_ds1302(addr);
+	// 3. data 전송
+	tx_ds1302(dec2bcd(data));
+	// 4. CE high ---> low
+	HAL_GPIO_WritePin(GPIOA, CE_DS1302_Pin, 0);
+}
+
+void tx_ds1302(uint8_t value)
+{
+	output_dataline_ds1302();
+	// ex) addr 초를 write하는
+	// 80h M       L
+	//     1000 0000	실제값	(B0를 전송시)
+	//     0000 0001 &
+	//	   0000 0000 ==> HAL_GPIO_WritePin(GPIOA, CE_DS1302_Pin, 0);
+	//     1000 0000	실제값	(B7를 전송시)
+	//     1000 0000  &		<< 7
+	//	   1000 0000 ==> HAL_GPIO_WritePin(GPIOA, CE_DS1302_Pin, 1);
+	for (int i=0; i<8; i++)
+	{
+		if (value & (1 << i))
+		{
+			HAL_GPIO_WritePin(GPIOA, IO_DS1302_Pin, 1);
+		}
+		else
+		{
+			HAL_GPIO_WritePin(GPIOA, IO_DS1302_Pin, 0);
+		}
+		clock_ds1302();
+	}
+}
+
+void output_dataline_ds1302()
+{
+	GPIO_InitTypeDef GPIO_init = {0,};	// {0,0,0,0,0}랑 같은 뜻 구조체 다 초기화
+
+	GPIO_init.Pin = IO_DS1302_Pin;
+	GPIO_init.Mode = GPIO_MODE_OUTPUT_PP;	// output mode
+	GPIO_init.Pull = GPIO_NOPULL;
+	GPIO_init.Speed = GPIO_SPEED_FREQ_HIGH; // LOW: 2M HIGH: 25~100MHz
+	HAL_GPIO_Init(GPIOA, &GPIO_init);		// 싱글변수이기 때문에 &로 주소를 넘김
+}
+
+void input_dataline_ds1302()
+{
+	GPIO_InitTypeDef GPIO_init = {0,};	// {0,0,0,0,0}랑 같은 뜻 구조체 다 초기화
+
+	GPIO_init.Pin = IO_DS1302_Pin;
+	GPIO_init.Mode = GPIO_MODE_INPUT;	// input mode
+	GPIO_init.Pull = GPIO_NOPULL;
+	HAL_GPIO_Init(GPIOA, &GPIO_init);		// 싱글변수이기 때문에 &로 주소를 넘김
+}
+
+
+void clock_ds1302()
+{
+	HAL_GPIO_WritePin(GPIOA, CLK_DS1302_Pin, 1);
+	HAL_GPIO_WritePin(GPIOA, CLK_DS1302_Pin, 0);
+}
+
+void init_gpio_ds1302()
+{
+	HAL_GPIO_WritePin(GPIOA, CE_DS1302_Pin, 0);
+	HAL_GPIO_WritePin(GPIOA, IO_DS1302_Pin, 0);
+	HAL_GPIO_WritePin(GPIOA, CLK_DS1302_Pin, 0);
+}
+
+void init_date_time()
+{
+	ds1302.year=25;
+	ds1302.month=3;
+	ds1302.date=28;			// .~ 직접 접근
+	ds1302.dayofweek=6;		// Fri
+	ds1302.hours=11;
+	ds1302.minutes=30;
+	ds1302.seconds=00;
+}
+
+
